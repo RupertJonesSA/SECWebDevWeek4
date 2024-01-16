3c46a52770b084a24cd6bd57f516ff83e079851f
RupertJonesSA <145080727+RupertJonesSA@users.noreply.github.com>
2024-01-15 20:38:12 -0500

    ch. 21 (useContext hook)

--- 07tut/src/NewPost.js
+++ 07tut/src/NewPost.js
@@ -1,6 +1,32 @@
-const NewPost = ({
-  handleSubmit, postTitle, setPostTitle, postBody, setPostBody
-}) => {
+import {useState, useContext} from 'react';
+import DataContext from './context/DataContext';
+import {useNavigate} from 'react-router-dom'; 
+import api from './api/posts'; 
+import { format } from 'date-fns'; 
+
+const NewPost = () => {
+    const [postTitle, setPostTitle] = useState(''); 
+    const [postBody, setPostBody] = useState('');
+    const { posts, setPosts} = useContext(DataContext); 
+    const navigate = useNavigate(); 
+    const handleSubmit = async (e) =>{
+      // POST operation 
+      e.preventDefault();
+      const id = posts.length ? posts[posts.length - 1].id + 1 : 1;
+      const datetime = format(new Date(), 'MMMM dd, yyyy pp'); 
+      const newPost = { id: `${id}`, title: postTitle, datetime, body: postBody };
+      try{
+        // simple axios method to do POST request to JSON server
+        const response = await api.post('/posts', newPost); 
+        const allPosts = [...posts, response.data]; 
+        setPosts(allPosts);
+        setPostTitle(''); 
+        setPostBody('');
+        navigate('/'); 
+      } catch(err){
+        console.log(`Error: ${err.message} `);
+      }
+    }
     return (
       <main className="NewPost">
           <h2>NewPost</h2>
